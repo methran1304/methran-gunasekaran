63c455c97a578d63c89f40b199cccc8628f0dace
Methran <methran2003@gmail.com>
2025-10-15 05:51:10 +0100

    commented blog code logic

--- src/app/services/blog-service.ts
+++ src/app/services/blog-service.ts
@@ -3,26 +3,26 @@ import { Injectable } from '@angular/core';
 import { Observable, Observer, Subscribable } from 'rxjs';
 import { Blog } from '../models/blog';
 import { BlogConstants } from '../constants/blog-constants';
-import { SECRETS } from '../../../secrets';
+// import { SECRETS } from '../../../secrets';
 import { GithubContentResponse } from '../models/github-content-response';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BlogService {
-  private githubToken = SECRETS.GithubAPIToken;
+  // private githubToken = SECRETS.GithubAPIToken;
 
-  constructor(private _httpClient: HttpClient) {}
+  // constructor(private _httpClient: HttpClient) {}
 
-  public getBlogContent(path: string = ''): Observable<GithubContentResponse> {
-    const url = `${BlogConstants.GetContentUrl}${path}`;
+  // public getBlogContent(path: string = ''): Observable<GithubContentResponse> {
+  //   const url = `${BlogConstants.GetContentUrl}${path}`;
 
-    return this._httpClient.get<GithubContentResponse>(url, {
-      headers: {
-        Accept: 'application/vnd.github.object',
-        Authorization: `Bearer ${this.githubToken}`,
-        'X-GitHub-Api-Version': '2022-11-28',
-      },
-    });
-  }
+  //   return this._httpClient.get<GithubContentResponse>(url, {
+  //     headers: {
+  //       Accept: 'application/vnd.github.object',
+  //       Authorization: `Bearer ${this.githubToken}`,
+  //       'X-GitHub-Api-Version': '2022-11-28',
+  //     },
+  //   });
+  // }
 }
