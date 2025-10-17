1a866839ec13c999271bdec09ce293b340b0bbf0
Methran <methran2003@gmail.com>
2025-10-17 21:22:43 +0100

    fixed syntax: routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -23,7 +23,7 @@ export const routes: Routes = [
     {
         path: 'blog',
         component: BlogContentComponent
-    }
+    },
     {
         path: 'projects',
         component: ProjectsComponent
