8b69d9cc0a6bdb3d146537500ef1a7ee15c98e28
Methran <methran2003@gmail.com>
2025-10-18 00:42:17 +0100

    added loader for blog pages

--- src/app/pages/blog/blog-content/blog-content.ts
+++ src/app/pages/blog/blog-content/blog-content.ts
@@ -15,22 +15,35 @@ export class BlogContentComponent implements OnInit {
   private slug!: string;
   markdownContent!: string;
 
+  isLoading: boolean = true;
+
   constructor(
     private _activatedRoute: ActivatedRoute,
     private _blogService: BlogService
   ) {}
 
   ngOnInit(): void {
+    this.getSlug();
+    this.getBlogContent();
+  }
+
+  getSlug(): void {
     this._activatedRoute.paramMap.subscribe((params) => {
       this.slug = params.get('slug')!;
     });
+  }
 
+  getBlogContent(): void {
     this._blogService.getBlogContent(this.slug).subscribe({
       next: (rawContent) => {
         const frontMatterRegex = /^---[\s\S]*?---/;
         this.markdownContent = rawContent.content.replace(frontMatterRegex, '').trim();
+        this.isLoading = false;
       },
-      error: (err) => {},
+      error: (err) => {
+        this.markdownContent = '';
+        this.isLoading = false;
+      },
     });
   }
 }
