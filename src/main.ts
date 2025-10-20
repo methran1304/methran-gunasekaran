65d8e2e0cc943f19173eaabbef434fd86286c53a
Methran <methran2003@gmail.com>
2025-10-20 02:45:08 +0100

    revert sanitize changes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideMarkdown, SANITIZE } from 'ngx-markdown';
+import { provideMarkdown } from 'ngx-markdown';
 import { App } from './app/app';
 import { routes } from './app/app.routes'; // create/export your routes
 import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { inject } from '@vercel/analytics';
-import { SecurityContext } from '@angular/core';
 
 // collect web analytics
 inject();
@@ -16,7 +15,6 @@ bootstrapApplication(App, {
     provideHttpClient(),
     provideMarkdown({
       loader: HttpClient,
-      sanitize: { provide: SANITIZE, useValue: SecurityContext.NONE },
     }),
     provideRouter(routes), // <-- important
   ],
