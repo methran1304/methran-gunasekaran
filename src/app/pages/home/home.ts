09d23aee9b1c624b90e33afdf7e9223ebea078d4
Methran <methran2003@gmail.com>
2026-03-16 00:46:07 +0000

    refactored tech stack icons

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,22 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import techStack from '../../data/tech-stack.json';
 import { NgxTypewriterComponent } from '@omnedia/ngx-typewriter';
 import { NgxCrypticTextComponent } from '@omnedia/ngx-cryptic-text';
-import { LucideAngularModule, FileUser, FolderOpen, NotebookPen } from 'lucide-angular';
+import {
+  LucideAngularModule,
+  FileUser,
+  FolderOpen,
+  NotebookPen,
+} from 'lucide-angular';
 import { RouterLink } from '@angular/router';
 import { ROUTE_CONSTANTS } from '../../constants/route-contants';
 
 @Component({
   selector: 'app-home',
   imports: [
+    CommonModule,
     NgxTypewriterComponent,
     NgxCrypticTextComponent,
     LucideAngularModule,
-    RouterLink
-],
+    RouterLink,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class HomeComponent {
+  languages = techStack.languages;
+  frameworks = techStack.frameworks;
+  tools = techStack.tools;
+
   readonly fileUserIcon = FileUser;
   readonly folderIcon = FolderOpen;
   readonly notebookPenIcon = NotebookPen;
